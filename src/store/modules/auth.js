62fe4d2d0e2990f316ef4aaa85ee0a1a28d29419
Prateek Varma <prtk.varma@gmail.com>
2022-02-01 21:11:07 -0600

    set the state and getters for auth module for nuxt

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,9 +1,12 @@
 const state = {
-
+    token: null
 };
 
 const getters = {
-
+    isLoggedIn: (state) => {
+        //the !! below will return 'true' or 'false' based on if 'state.token' is set or not
+        return !!state.token
+    }
 };
 
 const actions = {
