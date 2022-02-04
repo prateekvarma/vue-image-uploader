4b2656b021e9c3543b8e0dbac5ee316afd2192b4
Prateek Varma <prtk.varma@gmail.com>
2022-02-04 13:46:40 -0600

    Wired Vue-Router into the app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
 import store from './store';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [
+        {}
+    ]
+});
 
 new Vue({
     store,
