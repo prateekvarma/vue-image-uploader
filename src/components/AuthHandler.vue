d08107cdf1d00120f2ee0af462186cfdf66209c9
Prateek Varma <prtk.varma@gmail.com>
2022-02-07 21:01:29 -0600

    added the created() lifecycle hook to call the 'finalizeLogin' action

--- src/components/AuthHandler.vue
+++ src/components/AuthHandler.vue
@@ -7,6 +7,11 @@ import { mapActions } from 'vuex';
 
 export default {
     name: 'AuthHandler',
-    methods: mapActions(['finalizeLogin'])
+    methods: mapActions(['finalizeLogin']),
+    created() {
+        //this "this" keyword is important to use when calling an action from a lifecycle hook
+        this.finalizeLogin(window.location.hash);
+        //the 'window.location.hash' above will look into the URL, and take everything from starting of the # until the end of the URL
+    }
 }
 </script>
