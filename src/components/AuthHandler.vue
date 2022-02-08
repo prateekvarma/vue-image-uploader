5addf8b810cb1033d93a6460b2f2243a2e1c7a38
Prateek Varma <prtk.varma@gmail.com>
2022-02-07 21:25:35 -0600

    defined the 'finalizeLogin' action in the auth module of vuex

--- src/components/AuthHandler.vue
+++ src/components/AuthHandler.vue
@@ -11,7 +11,7 @@ export default {
     created() {
         //this "this" keyword is important to use when calling an action from a lifecycle hook
         this.finalizeLogin(window.location.hash);
-        //the 'window.location.hash' above will look into the URL, and take everything from starting of the # until the end of the URL
+        //the 'window.location.hash' above will look into the URL, and take everything from starting of the # until the end of the URL. This param is going to show up as the 2nd argument where the action is defined. The 1st argument is a bunch of helper functions always (at the function definition)
     }
 }
 </script>
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,5 @@
 import api from '../../api/imgur';
+import qs from 'qs';
 
 const state = {
     token: null
@@ -20,6 +21,15 @@ const actions = {
     login: () => {
         api.login();
     },
+    //the 'hash' below = 'window.location.hash' from file AuthHandler.vue
+    finalizeLogin: ({commit}, hash) => {
+        //The hash contains a long URL string, with a lot of info, including some variables and values.
+        //Below the "hash.replace('#', '')" will remove the # and replace it with ''
+        const query = qs.parse(hash.replace('#', ''));
+        //the qs above is a library, the parse is a function in the qs library, that take a string and separates out it's key value pairs. This will be then stored in the 'query' variable
+        commit('setToken', query.access_token);
+        //below, we're calling the setToken mutation, and passing the token value as 'query.access_token' -> which is a key-value pair's key, taken from the URL
+    }
 };
 
 const mutations = {
