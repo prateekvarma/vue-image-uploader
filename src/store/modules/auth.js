3aeae8fc2fa701cc87e4c77e96159026838c25a1
Prateek Varma <prtk.varma@gmail.com>
2022-02-13 18:44:12 -0600

    added login status to localStorage, which is native storage to the browser

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -28,7 +28,9 @@ const actions = {
         const query = qs.parse(hash.replace('#', ''));
         //the qs above is a library, the parse is a function in the qs library, that take a string and separates out it's key value pairs. This will be then stored in the 'query' variable
         commit('setToken', query.access_token);
-        //below, we're calling the setToken mutation, and passing the token value as 'query.access_token' -> which is a key-value pair's key, taken from the URL
+        //above, we're calling the setToken mutation, and passing the token value as 'query.access_token' -> which is a key-value pair's key, taken from the URL
+        window.localStorage.setItem('imgur_token', query.access_token);
+        //above, we're setting a variable named 'imgur_token' into the localStorage of the native browser. This way, we can access this variable & sustain login status of the user even when he goes to other URLs and returns back to find his login status intact.
     }
 };
 
