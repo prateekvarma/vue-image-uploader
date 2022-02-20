eed45411a95ff7c8508762fd27d2ac20d4887e15
Prateek Varma <prtk.varma@gmail.com>
2022-02-20 15:03:27 -0600

    added 2 more components(ImageList & UploadForm), and hooked the corresponding routes

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import VueRouter from 'vue-router';
 import App from './App';
 import store from './store';
 import AuthHandler from './components/AuthHandler.vue';
+import ImageList from './components/ImageList.vue';
+import UploadForm from './components/UploadForm.vue';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
     mode: 'history',
-    routes: [{ path: '/oauth2/callback', component: AuthHandler }]
+    routes: [
+        { path: '/', component: ImageList },
+        { path: '/upload', component: UploadForm },
+        { path: '/oauth2/callback', component: AuthHandler },
+    ]
 });
 
 new Vue({
