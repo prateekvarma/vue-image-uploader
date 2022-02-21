9f527ae22d9e57c750a30545443711106c7cd687
Prateek Varma <prtk.varma@gmail.com>
2022-02-21 16:50:33 -0600

    called fetchImages function from created() lifecycle hook in ImageList component

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -5,7 +5,13 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
     export default {
-        name: 'ImageList'
+        name: 'ImageList',
+        //below, mapActions is the only way to call actions, and then the created() lifecycle hook will call the function every time this component will be rendered.
+        methods: mapActions(['fetchImages']),
+        created() {
+            this.fetchImages();
+        }
     };
 </script>
--- src/store/modules/images.js
+++ src/store/modules/images.js
@@ -2,7 +2,7 @@ import api from '../../api/imgur.js';
 
 const state = {
     //below images is an array, which will have different objects, each object will represent a different image.
-    images = []
+    images : []
 };
 
 const getters = {
@@ -22,7 +22,7 @@ const actions = {
 
 const mutations = {
     //below, is a standard format of any mutation function. The first argument is always the state & the 2nd argument is always a parameter that is passed to it.
-    setImages = (state, images) => {
+    setImages: (state, images) => {
         //below, the passed object 'images' is assigned as the new state object.
         state.images = images;
     }
