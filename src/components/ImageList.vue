7555c92c69d400f2e53c7c22be7bd8798744fced
Prateek Varma <prtk.varma@gmail.com>
2022-02-28 21:50:30 -0600

    Temporary code to display images in component

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
-        Image List
-        {{ allImages.length }}
+        <img v-for="image in allImages" v-bind:src="image.link" v-bind:key="image.id" />
     </div>
 </template>
 
