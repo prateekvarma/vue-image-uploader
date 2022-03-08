ea559d0b11023389e937ce7b55878d0b194b0c84
Prateek Varma <prtk.varma@gmail.com>
2022-03-07 22:14:11 -0600

    corrected commments for router-view

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div>
         <AppHeader />
         <div class="ui container">
-            /* router-view displays the content when some router URL is clicked */
+            <!-- router-view displays the content when some router URL is clicked -->
             <router-view></router-view>
         </div>
     </div>    
